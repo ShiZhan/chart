f33dfae8471b6af865c402124395749f50bd997a
ShiZhan <g.shizhan.g@gmail.com>
2013-12-07 21:59:19 -0600

    moving from trigram

---
+++ project/Build.scala
@@ -0,0 +1,29 @@
+import sbt._
+import Keys._
+
+object MyBuild extends Build {
+  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
+    version := "0.1-SNAPSHOT",
+    organization := "com.simba",
+    scalaVersion := "2.10.3"
+  )
+
+  lazy val copyDependencies = TaskKey[Unit]("copy-dep")
+
+  def copyDepTask = copyDependencies <<= (update, crossTarget, scalaVersion) map {
+    (updateReport, out, scalaVer) =>
+    updateReport.allFiles foreach { srcPath =>
+      val destPath = out / "lib" / srcPath.getName
+      IO.copyFile(srcPath, destPath, preserveLastModified=true)
+    }
+  }
+
+  lazy val chart = Project(
+    id = "chart",
+    base = file("."),
+    settings = Defaults.defaultSettings ++
+    sbtassembly.Plugin.assemblySettings ++ Seq(
+      copyDepTask
+    )
+  )
+}
