601745a9d43dc6bca449b57e959cdeac602cf6cc
ShiZhan <g.shizhan.g@gmail.com>
2014-08-20 22:51:59 +0800

    barcharts

--- src/main/scala/chart.scala
+++ src/main/scala/chart.scala
@@ -61,53 +61,46 @@ object multilinechart {
   }
 }
 
-//object barchart {
-//  import helper.RenderEx.PatternRenderer
-//  import helper.Config.CHARTDATA
-//
-//  def main(args: Array[String]) = {
-//    val data = Seq(
-//      ("A", "s0", 1), ("B", "s0", 3), ("C", "s0", 5),
-//      ("D", "s0", 7), ("E", "s0", 9))
-//    val dataset = data.toCategoryDataset
-//    val bChart = BarChart(
-//      dataset,
-//      title = "Example Chart of Some Bars",
-//      domainAxisLabel = "X",
-//      rangeAxisLabel = "Y")
-//    val jfc = bChart.peer
-//    jfc.getLegend.setPosition(RectangleEdge.RIGHT)
-//    jfc.backgroundPaint = Color.white
-//    val plot = bChart.plot
-//    plot.setBackgroundPaint(Color.white)
-//    plot.setRangeGridlinePaint(Color.blue)
-//    plot.setRenderer(new PatternRenderer)
-//    bChart.show
-//    bChart.saveAsPDF(CHARTDATA + "/barchart.pdf", (500, 375))
-//  }
-//}
-//
-//object multibarchart {
-//  import helper.RenderEx.PatternRenderer
-//  import helper.Config.CHARTDATA
-//
-//  def main(args: Array[String]) = {
-//    val data = List(
-//      ("C1", "A", 3), ("C1", "B", 4), ("C1", "C", 3),
-//      ("C2", "A", 7), ("C2", "B", 8), ("C2", "C", 8))
-//    val dataset = data.toCategoryDataset
-//    val bChart = BarChart(
-//      dataset,
-//      domainAxisLabel = "X",
-//      rangeAxisLabel = "Y")
-//    val jfc = bChart.peer
-//    jfc.getLegend.setPosition(RectangleEdge.RIGHT)
-//    jfc.backgroundPaint = Color.white
-//    val plot = bChart.plot
-//    plot.setBackgroundPaint(Color.white)
-//    plot.setRangeGridlinePaint(Color.lightGray)
-//    plot.setRenderer(new PatternRenderer)
-//    bChart.show
-//    bChart.saveAsPDF(CHARTDATA + "/multibarchart.pdf", (500, 375))
-//  }
-//}
+object barchart {
+  import helper.RenderEx.PatternRenderer
+  import helper.Config.CHARTDATA
+
+  def main(args: Array[String]) = {
+    val data = Vector("s0" -> Vector.tabulate(5)(i => ((i + 'A').toChar, Random.nextInt(10))))
+    val bChart = BarChart(data)
+    bChart.plot.getDomainAxis.setLabel("X")
+    bChart.plot.getRangeAxis.setLabel("Y")
+    val jfc = bChart.peer
+    jfc.getLegend.setPosition(RectangleEdge.RIGHT)
+    jfc.setBackgroundPaint(Color.white)
+    val plot = bChart.plot
+    plot.setBackgroundPaint(Color.white)
+    plot.setRangeGridlinePaint(Color.blue)
+    plot.setRenderer(new PatternRenderer)
+    bChart.show("Example Chart of Some Bars", (500, 375), false)
+    bChart.saveAsPDF(CHARTDATA + "/barchart.pdf", (500, 375))
+  }
+}
+
+object multibarchart {
+  import helper.RenderEx.PatternRenderer
+  import helper.Config.CHARTDATA
+
+  def main(args: Array[String]) = {
+    val data = Vector(
+      "C1" -> Vector.tabulate(3)(i => ((i + 'A').toChar, Random.nextInt(10))),
+      "C2" -> Vector.tabulate(3)(i => ((i + 'A').toChar, Random.nextInt(10))))
+    val bChart = BarChart(data)
+    bChart.plot.getDomainAxis.setLabel("X")
+    bChart.plot.getRangeAxis.setLabel("Y")
+    val jfc = bChart.peer
+    jfc.getLegend.setPosition(RectangleEdge.RIGHT)
+    jfc.setBackgroundPaint(Color.white)
+    val plot = bChart.plot
+    plot.setBackgroundPaint(Color.white)
+    plot.setRangeGridlinePaint(Color.lightGray)
+    plot.setRenderer(new PatternRenderer)
+    bChart.show("Example Chart of Some Bars", (500, 375), false)
+    bChart.saveAsPDF(CHARTDATA + "/multibarchart.pdf", (500, 375))
+  }
+}
